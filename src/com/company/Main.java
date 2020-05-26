56a9ff4c2bf7817857f341460766d9dd44b49e66
djshampz <gitau.marto@gmail.com>
2020-05-26 18:45:11 +0300

    second commit(group memmbers)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ package com.company;
     Group Members
     100994 Kimani Martin Gitau
     088462 Alvin Moses
-
+    110401 Victoria Mutwiri
 */
 public class Main{
     public static void main(String[] args) {
